2d6263a94b608a95886963f3e85c1f590151e7c1
Thiago Brito <thiagobrito@Thiagos-MacBook-Pro.local>
2021-06-17 22:15:29 -0300

    Strapi commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '221cf30f2673b96c71ab20164960c212'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e608132b-5d94-463b-8d7d-37f592e18e83'
+};
