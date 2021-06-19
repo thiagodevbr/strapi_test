23f5607883d77c6d6da726d7e478445498b1eee5
Thiago Brito <britotr@gmail.com>
2021-06-19 20:51:15 -0300

    netlify webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -4,5 +4,16 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
+const axios = require('axios')
+const netlifyWebHook = strapi.config.get('custom.netlifyWebHook')
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebHook && axios.post(netlifyWebHook)
+    },
+    async afterUpdate(_, __, ___) {
+      netlifyWebHook && axios.post(netlifyWebHook)
+    }
+  }
+};
--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
     provider: 'scaleway',
     providerOptions: {
@@ -12,5 +11,4 @@ module.exports = ({ env }) => ({
       }
     }
   },
-  // ...
 });
